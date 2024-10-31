b14cfcf617662bbf3a5dfbcbd5fc2076394bf7de
pcastellanos-minka <paula@minka.io>
2024-10-31 18:34:53 +0000

    adding dev container

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+npm install -g typescript ts-node nodemon
+
+cd src
+npm ci
+
+cd ../client
+npm ci
+
+cd ..
--- src/routes/nonSDK/createIntentNonSDK.ts
+++ src/routes/nonSDK/createIntentNonSDK.ts
@@ -5,6 +5,8 @@ import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config({ path: `${__dirname}/../../.env` })
 import { config } from '../../config'
+import moment from 'moment';
+
 
 const HASHING_ALGORITHM = 'sha256';
 
